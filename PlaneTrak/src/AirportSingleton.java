58249b102f94858f58daa491a0edebeb5961e805
Sean Hart <seanharteng@gmail.com>
2022-05-03 23:16:52 -0400

    Add singleton class and demonstrate its usage in main

---
+++ PlaneTrak/src/AirportSingleton.java
@@ -0,0 +1,100 @@
+import planes.CargoPlane;
+import planes.PassengerPlane;
+import planes.PlaneIF;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AirportSingleton {
+
+    private static AirportSingleton airport = null;
+
+    private List<PlaneIF> planesAtAirport = new ArrayList<>();
+
+    private int cargoAtAirport;
+
+    private int passengersAtAirport;
+    private AirportSingleton(){
+
+    }
+
+    public static AirportSingleton getAirport(){
+        if(airport == null){
+            airport = new AirportSingleton();
+        }
+        return airport;
+    }
+
+    public void addPlaneToAirport(PlaneIF plane){
+        if(plane instanceof CargoPlane){
+            plane = (CargoPlane) plane;
+            addCargoToAirport(((CargoPlane) plane).getCargoTonnage());
+            ((CargoPlane) plane).setCargoTonnage(0);
+        }else{
+            plane = (PassengerPlane) plane;
+            addPassengersToAirport(((PassengerPlane) plane).getPassengerCount());
+            ((PassengerPlane) plane).setPassengerCount(0);
+        }
+        planesAtAirport.add(plane);
+    }
+
+    public PlaneIF removePlaneFromAirport(int arrayListSpot, int planeCarryValue){
+        if(planesAtAirport.size() == 0){
+            System.out.println("No planes at airport.");
+            return null;
+        }
+
+        PlaneIF plane = planesAtAirport.get(arrayListSpot);
+        if(plane instanceof CargoPlane){
+            if(getCargoAtAirport() < planeCarryValue){
+                System.out.println("Not enough cargo at airport.");
+                return null;
+            }
+            removeCargoFromAirport(planeCarryValue);
+            ((CargoPlane) plane).setCargoTonnage(planeCarryValue);
+        }else{
+            if(passengersAtAirport < planeCarryValue){
+                System.out.println("Not enough passengers at airport.");
+                return null;
+            }
+            removePassengersFromAirport(planeCarryValue);
+            ((PassengerPlane) plane).setPassengerCount(planeCarryValue);
+        }
+        planesAtAirport.remove(arrayListSpot);
+        return plane;
+    }
+
+    public int getCargoAtAirport(){
+        return cargoAtAirport;
+    }
+
+    public void addCargoToAirport(int cargo){
+        this.cargoAtAirport += cargo;
+    }
+
+    public void removeCargoFromAirport(int cargo){
+        this.cargoAtAirport -= cargo;
+    }
+
+    public int getPassengersAtAirport(){
+        return passengersAtAirport;
+    }
+
+    public void addPassengersToAirport(int passengers){
+        this.passengersAtAirport += passengers;
+    }
+
+    public void removePassengersFromAirport(int passengers){
+        this.passengersAtAirport -= passengers;
+    }
+
+    public void printListOfPlanes(){
+        System.out.println(planesAtAirport.toString());
+    }
+
+
+
+
+
+
+}
--- PlaneTrak/src/Main.java
+++ PlaneTrak/src/Main.java
@@ -1,6 +1,57 @@
+import planefactories.AirbusA380Factory;
+import planefactories.Boeing757Factory;
+import planefactories.CessnaCitationXFactory;
+import planefactories.PlaneFactoryIF;
+import planes.PlaneIF;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //create singleton instance
+        AirportSingleton myAirportSingleton = AirportSingleton.getAirport();
+
+        //create plane factories
+        PlaneFactoryIF myAirbusFactory = new AirbusA380Factory();
+        PlaneFactoryIF myCessnaFactory = new CessnaCitationXFactory();
+        PlaneFactoryIF myBoeingFactory = new Boeing757Factory();
+
+        //create planes for test
+        PlaneIF myAirbus = myAirbusFactory.createPlane(400);
+        PlaneIF myCessna = myCessnaFactory.createPlane(10);
+        PlaneIF myBoeing = myBoeingFactory.createPlane(6000);
+
+        //add planes to airport
+        myAirportSingleton.addPlaneToAirport(myAirbus);
+        myAirportSingleton.addPlaneToAirport(myCessna);
+        myAirportSingleton.addPlaneToAirport(myBoeing);
+
+        //print cargo and passenger counts after planes created above have arrived
+        System.out.println("Current Passengers Count at airport: " + myAirportSingleton.getPassengersAtAirport());
+        System.out.println("Current Cargo Tonnage at airport: " + myAirportSingleton.getCargoAtAirport());
+
+        //print planes at airport
+        myAirportSingleton.printListOfPlanes();
+
+        //demonstrate that if there are too few passengers or not enough cargo at airport,
+        // the function handles correctly
+        myAirportSingleton.removePlaneFromAirport(0, 600);
+        myAirportSingleton.removePlaneFromAirport(2, 10000);
+
+        myAirportSingleton.removePlaneFromAirport(0, 200);
+        myAirportSingleton.printListOfPlanes();
+        myAirportSingleton.removePlaneFromAirport(0, 20);
+        myAirportSingleton.printListOfPlanes();
+        myAirportSingleton.removePlaneFromAirport(0, 2500);
+
+        //Print planes and cargo now that planes have left airport
+        myAirportSingleton.printListOfPlanes();
+        System.out.println("Current Passengers Count at airport: " + myAirportSingleton.getPassengersAtAirport());
+        System.out.println("Current Cargo Tonnage at airport: " + myAirportSingleton.getCargoAtAirport());
+
+
+
+
+
 
     }
 }
